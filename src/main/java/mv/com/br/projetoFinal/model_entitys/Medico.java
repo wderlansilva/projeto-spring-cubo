4ea2fac5ebaa2ca0acd400894717678b738a72f5
Marcos Felipe <marcos.vasconcelos@mv.com.br>
2022-08-09 09:04:20 -0300

    Criando classe do Medico

--- src/main/java/mv/com/br/projetoFinal/model_entitys/Medico.java
+++ src/main/java/mv/com/br/projetoFinal/model_entitys/Medico.java
@@ -1,4 +1,53 @@
 package mv.com.br.projetoFinal.model_entitys;
 
 public class Medico {
+    public Long id;
+    public String nome;
+    public String hospital;
+    public Long crm;
+
+    public Medico(Long id, String nome, String hospital, Long crm) {
+        this.id = id;
+        this.nome = nome;
+        this.hospital = hospital;
+        this.crm = crm;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getHospital() {
+        return hospital;
+    }
+
+    public void setHospital(String hospital) {
+        this.hospital = hospital;
+    }
+
+    public Long getCrm() {
+        return crm;
+    }
+
+    public void setCrm(Long crm) {
+        this.crm = crm;
+    }
+
+
+
 }
+
+
+
