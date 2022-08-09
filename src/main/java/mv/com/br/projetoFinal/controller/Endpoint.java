7c6444d6b14c48d9d4168502015947bbafde4bbb
Marcos Felipe <marcos.vasconcelos@mv.com.br>
2022-08-09 10:16:04 -0300

    Criando Medico DTO e configurando primeiro endpoint

--- src/main/java/mv/com/br/projetoFinal/controller/Endpoint.java
+++ src/main/java/mv/com/br/projetoFinal/controller/Endpoint.java
@@ -1,5 +1,18 @@
 package mv.com.br.projetoFinal.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/pacientes")
 public class Endpoint {
+
+    @GetMapping("/get")
+    public String ola(){
+        return "Olaa";
+    }
 
 }
